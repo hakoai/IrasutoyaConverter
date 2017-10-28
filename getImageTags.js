0e4c12a64cd353344bba1098bd84390af89f073f
92thunder <r.kunisada661@gmail.com>
2017-10-28 21:06:02 +0900

    Fix to use tags in computer vision response.

--- getImageTags.js
+++ getImageTags.js
@@ -4,7 +4,7 @@ var options = {
   "method": "POST",
   "hostname": "westus.api.cognitive.microsoft.com",
   "port": null,
-  "path": "/vision/v1.0/analyze?oauth_consumer_key=" + process.env.COMPUTER_VISION_API_KEY1,
+  "path": "/vision/v1.0/analyze?visualFeatures=Tags&oauth_consumer_key=" + process.env.COMPUTER_VISION_API_KEY1,
   "headers": {
     "content-type": "application/json",
     "ocp-apim-subscription-key": process.env.COMPUTER_VISION_API_KEY2,
@@ -23,8 +23,7 @@ function getImageTags(imageURL, callback) {
     res.on("end", function () {
       var body = Buffer.concat(chunks);
       var data = JSON.parse(body);
-      var resultMap = Array.prototype.concat.apply([],
-        data["categories"].map(category => category.name.split('_')));
+      var resultMap = data["tags"].map(element => element["name"]);
       callback(resultMap);
     });
   });
